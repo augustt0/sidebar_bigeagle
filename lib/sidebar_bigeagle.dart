fb0676cb0bdd34f1888febf165ac32cfdeb0d0f0
augustt0 <48627057+augustt0@users.noreply.github.com>
2021-03-12 01:26:44 -0300

    main error fix

--- lib/sidebar_bigeagle.dart
+++ lib/sidebar_bigeagle.dart
@@ -3,3 +3,131 @@ library sidebar_bigeagle;
 export 'package:sidebar_bigeagle/src/SideBar.dart';
 export 'package:sidebar_bigeagle/src/SideBarButton.dart';
 export 'package:sidebar_bigeagle/src/SideBarButtonFlat.dart';
+
+import 'package:animated_size_and_fade/animated_size_and_fade.dart';
+import 'package:flutter/material.dart';
+
+import 'package:sidebar_bigeagle/src/SideBarButton.dart';
+import 'package:sidebar_bigeagle/src/SideBarButtonFlat.dart';
+
+typedef IntCallback(int);
+
+class SideBar extends StatefulWidget {
+  SideBar({Key key, @required this.children, @required this.logo, this.color, this.onChange}) : super(key: key);
+  List<SideBarButtonFlat> children;
+  int selectedIndex = 0;
+  final IntCallback onChange;
+  Image logo;
+  Color color;
+  @override
+  _SideBarState createState() => _SideBarState(children: children, logo: logo, color: color);
+}
+
+class _SideBarState extends State<SideBar> with TickerProviderStateMixin{
+  _SideBarState({@required this.children, this.logo, this.color});
+
+  Image logo;
+  Color color;
+
+  static int _selectedIndex = 0;
+
+  List<SideBarButtonFlat> children;
+
+  List<Widget> _selectedButtonOpen = new List<Widget>();
+  List<Widget> _selectedButtonClosed = new List<Widget>();
+
+  updateButton(int x) {
+    setState(() {
+      _selectedIndex = x;
+      widget.onChange(x);
+    });
+  }
+
+  void initializeWidgets() {
+    children.forEach((element) {
+      SideBarButton sideBarButtonOpen = new SideBarButton(
+        title: element.title,
+        icon: element.icon,
+        pressed: true,
+        index: children.indexOf(element),
+      );
+
+      SideBarButton sideBarButtonClose = new SideBarButton(
+        title: element.title,
+        icon: element.icon,
+        pressed: false,
+        index: children.indexOf(element),
+        updateValue: updateButton,
+      );
+
+      _selectedButtonOpen.add(sideBarButtonOpen);
+      _selectedButtonClosed.add(sideBarButtonClose);
+    });
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    initializeWidgets();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        width: MediaQuery.of(context).size.width / 6 + 40,
+        height: MediaQuery.of(context).size.height,
+        padding: EdgeInsets.zero,
+        child: Stack(
+          children: [
+            Positioned(
+              left: -5,
+              top: -5,
+              bottom: -5,
+              child: Container(
+                padding: EdgeInsets.zero,
+                width: MediaQuery.of(context).size.width / 6 + 40,
+                height: MediaQuery.of(context).size.height,
+                child: Card(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.only(
+                          topRight: Radius.circular(35),
+                          bottomRight: Radius.circular(35))),
+                  elevation: 0.0,
+                  color: color,
+                ),
+              ),
+            ),
+            Positioned(
+              left: 10,
+              top: MediaQuery.of(context).size.height / 4,
+              bottom: 0,
+              child: Container(
+                width: MediaQuery.of(context).size.width / 6 + 40,
+                height: MediaQuery.of(context).size.height,
+                child: ListView.builder(
+                  itemCount: _selectedButtonOpen.length,
+                  shrinkWrap: true,
+                  physics: BouncingScrollPhysics(),
+                  itemBuilder: (context, i) {
+                    return AnimatedSizeAndFade(
+                      vsync: this, 
+                      child: _selectedIndex == i ? _selectedButtonOpen[i] : _selectedButtonClosed[i],
+                      fadeDuration: const Duration(milliseconds: 50),
+                      sizeDuration: const Duration(milliseconds: 200),
+                    );
+                  }),
+              )
+            ),
+            Positioned(
+              top: 40,
+              left: 10,
+              child: Container(
+                width: MediaQuery.of(context).size.width / 6 + 10,
+                child: logo,
+                ),
+            ),
+          ],
+        ));
+  }
+}
